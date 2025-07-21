ab473b536dd390f0cc13e72a69bc712426ca2da2
Gennadiy Strelkov <jellyfishbountyhunter@gmail.com>
2025-07-21 02:49:12 -0400

    Project 12 Submission 1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const clothingItemsRouter = require("./clothingItems");
 const userRouter = require("./users");
 
-//router.use("/clothing-items", clothingItemsRouter);
+// router.use("/clothing-items", clothingItemsRouter);
 router.use("/items", clothingItemsRouter);
 router.use("/users", userRouter);
 
